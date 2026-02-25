c417c6db54240f281d129956c1ff5a089a86578e
edhoblyn <edhoblyn@gmail.com>
2026-02-25 12:47:30 +0000

    added restaurant controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,22 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates :rating, presence: true
-
-  validate :rating_must_be_integer_between_0_and_5
-
-  private
-
-  def rating_must_be_integer_between_0_and_5
-    return if rating.nil?
-
-    unless rating.is_a?(Integer)
-      errors.add(:rating, "must be an integer")
-      return
-    end
-
-    unless rating.between?(0, 5)
-      errors.add(:rating, "must be between 0 and 5")
-    end
-  end
+  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
 end
